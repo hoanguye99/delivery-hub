d77726de68c2e146ae1cd108347f2c3d0abdca99
Hoang Nguyen <hdnguyen0305@gmail.com>
2024-12-25 15:14:27 +0700

    edit button ui

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -10,6 +10,8 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        light:
+          "bg-primary-background text-primary hover:bg-primary-background/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
@@ -23,7 +25,7 @@ const buttonVariants = cva(
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
+        icon: "h-10 w-10 rounded-full aspect-square",
       },
     },
     compoundVariants: [
