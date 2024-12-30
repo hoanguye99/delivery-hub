d2eedaca48e80754135bb9dfcb7f050fa8561cd2
Hoang Nguyen <hdnguyen0305@gmail.com>
2024-12-30 17:32:07 +0700

    login form half done

--- components/login/log-in-form/hook.ts
+++ components/login/log-in-form/hook.ts
@@ -1,26 +1,61 @@
 import { zodResolver } from "@hookform/resolvers/zod"
+import { useMutation } from "@tanstack/react-query"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
+import { AxiosError } from "axios"
+import axiosClient from "@/apis/axios-client"
+import { toast } from "sonner"
 
-const formSchema = z.object({
+const loginFormSchema = z.object({
   email: z.string().email(),
-  password: z
-    .string()
-    .min(8, { message: "Password must be at least 8 characters long" }),
+  password: z.string().min(1, { message: "Password cannot be empty" }),
 })
 
+// 👀 API Response
+const UserDetailSchema = z.object({
+  id: z.string(),
+  fullname: z.string(),
+  email: z.string(),
+  tenant: z.string(),
+  accessToken: z.string(),
+  refreshToken: z.string(),
+})
+
+export type UserDetail = z.infer<typeof UserDetailSchema>
+
+const loginAPI = async (data: z.infer<typeof loginFormSchema>) => {
+  const url = "/auth/login/"
+  const response = await axiosClient.post(url, data)
+  return UserDetailSchema.parse(response)
+}
+
 export const useLoginForm = () => {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof loginFormSchema>>({
+    resolver: zodResolver(loginFormSchema),
     defaultValues: {
       email: "",
       password: "",
     },
   })
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  const mutation = useMutation<
+    UserDetail,
+    AxiosError,
+    z.infer<typeof loginFormSchema>
+  >({
+    mutationFn: (formData) => loginAPI(formData),
+    onSuccess: (data, variables, context) => {
+      console.log("success")
+      console.log(data)
+    },
+    // onError: (error) => {
+    //   console.error(error)
+    // },
+  })
+
+  function onSubmit(values: z.infer<typeof loginFormSchema>) {
     // ✅ This will be type-safe and validated.
-    console.log(values)
+    mutation.mutate(values)
   }
 
   return { form, onSubmit }
